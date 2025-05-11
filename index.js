b4030b3ceb72b5624ee5fb0f84a75ab02179c8e9
risonarian <risonarian@outlook.jp>
2025-05-11 10:39:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // publicフォルダ内のファイルを静的に提供
 app.use(express.static('public'));
